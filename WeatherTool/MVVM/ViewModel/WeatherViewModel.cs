0fb5d399af10bafb38c6973e1209492123127bf4
bulgos <dchristev@gmail.com>
2022-02-01 17:36:41 +0100

    Add project files.

---
+++ WeatherTool/MVVM/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherTool.MVVM.ViewModel
+{
+    public class WeatherViewModel : BindableBase
+    {
+        public WeatherViewModel() { }
+    }
+}
