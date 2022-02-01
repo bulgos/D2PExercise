903c9380e55ce19c9c3dca4d40f9174ad7c8b18e
bulgos <dchristev@gmail.com>
2022-02-01 18:00:42 +0100

    Add WeatherDay Model for MVVM

---
+++ WeatherTool/MVVM/Model/WeatherDay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherTool.MVVM.Model
+{
+    public class WeatherDay : BindableBase
+    {
+        private DateOnly _date;
+        private int _lowTemp;
+        private int _highTemp;
+
+        public WeatherDay(DateOnly date, int lowTemp, int highTemp)
+        {
+            _date = date;
+            _lowTemp = lowTemp;
+            _highTemp = highTemp;
+        }
+
+        public DateOnly Date
+        {
+            get => _date;
+            set => SetProperty(ref _date, value);
+        }
+
+        public int LowTemp
+        {
+            get => _lowTemp;
+            set => SetProperty(ref _lowTemp, value);
+        }
+
+        public int HighTemp 
+        { 
+            get => _highTemp; 
+            set => SetProperty(ref _highTemp, value);
+        }
+    }
+}
--- WeatherTool/MVVM/ViewModel/WeatherViewModel.cs
+++ WeatherTool/MVVM/ViewModel/WeatherViewModel.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherTool.MVVM.Model;
 
 namespace WeatherTool.MVVM.ViewModel
 {
     public class WeatherViewModel : BindableBase
     {
+        private WeatherDay _weatherDay;
+
         public WeatherViewModel() { }
+
+        public WeatherDay WeatherDay 
+        { 
+            get => _weatherDay; 
+            set => _weatherDay = value; 
+        }
     }
 }
