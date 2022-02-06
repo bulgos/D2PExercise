3b9dc9fec0fa77aabba68780dc80612f19f5832d
bulgos <dchristev@gmail.com>
2022-02-06 23:54:06 +0100

    Add ListCollectionView for displaying weather

--- WeatherTool/MVVM/ViewModel/WeatherViewModel.cs
+++ WeatherTool/MVVM/ViewModel/WeatherViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using WeatherTool.MVVM.Model;
 
 namespace WeatherTool.MVVM.ViewModel
@@ -11,13 +12,41 @@ namespace WeatherTool.MVVM.ViewModel
     public class WeatherViewModel : BindableBase
     {
         private WeatherDay _weatherDay = new WeatherDay();
+        private ListCollectionView _weatherCollection;
+        
+        
 
-        public WeatherViewModel() { }
+        public WeatherViewModel()
+        {
+            BuildDefaultCollection();
+        }
 
-        public WeatherDay WeatherDay 
-        { 
-            get => _weatherDay; 
-            set => _weatherDay = value; 
+        public WeatherDay WeatherDay
+        {
+            get => _weatherDay;
+            set => _weatherDay = value;
+        }
+
+        public ListCollectionView WeatherCollection
+        {
+            get => _weatherCollection;
+            set => SetProperty(ref _weatherCollection, value);
+        }
+
+        private void BuildDefaultCollection()
+        {
+            List<WeatherDay> weatherList = new List<WeatherDay>();
+
+            var dateToday = DateOnly.FromDateTime(DateTime.Now);
+
+            for (int i = 0; i < 7; i++)
+            {
+                var date = dateToday.AddDays(-i);
+                WeatherDay weatherDay = new WeatherDay(date, 10, 15);
+                weatherList.Add(weatherDay);
+            }
+
+            WeatherCollection = new ListCollectionView(weatherList);
         }
     }
 }
